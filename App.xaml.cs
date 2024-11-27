8b4be02f74b7926e61963149eba162bd2ce632b0
ALINA-VERONICA FĂT <alina.fat@stud.ubbcluj.ro>
2024-11-27 22:57:56 +0200

    q

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace AlinaFat
+﻿using System;
+using AlinaFat.Data;
+using System.IO;
+
+namespace AlinaFat
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
---
+++ Models/ShopList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using SQLite;
+
+namespace AlinaFat.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        // Specifică namespace-ul complet pentru MaxLength din DataAnnotations
+        [System.ComponentModel.DataAnnotations.MaxLength(250), Unique]
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
