d3696fe28569b3bc9606b4aec1c9a46f29e84ed2
ALINA-VERONICA FĂT <alina.fat@stud.ubbcluj.ro>
2024-12-05 19:23:23 +0200

    aa

---
+++ ProductPage.xaml.cs
@@ -0,0 +1,36 @@
+using AlinaFat.Models;
+
+namespace AlinaFat;
+
+public partial class ProductPage : ContentPage
+    ShopList sl;
+{
+    public ProductPage(ShopList slist)
+    {
+        InitializeComponent();
+    sl = slist;
+}
+
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.SaveProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var product = listView.SelectedItem as Product;
+        if (product != null)
+        {
+            await App.Database.DeleteProductAsync(product);
+            listView.ItemsSource = await App.Database.GetProductsAsync();
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+}
